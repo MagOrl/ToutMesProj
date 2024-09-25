dcaa0785220b5049d13bc1b172be2156eba948e3
MagomedORLEANS <magomed.arsamerzoev@etu.univ-orleans.fr>
2024-09-25 14:04:32 +0200

    caca

--- INNIT_PROJ/TP/sante.py
+++ INNIT_PROJ/TP/sante.py
@@ -1,10 +1,19 @@
-def sante(taille, poids):
-    """_summary_
+W = {
+    "rock": ["lizard", "scissors"],
+    "paper": ["spock", "rock"],
+    "scissors": ["paper", "lizard"],
+    "lizard": ["paper", "spock"],
+    "spock": ["rock", "scissors"],
+}
+def rpsls(p1, p2):
+    p1 = p1.lower()
+    p2 = p2.lower()
+    if p1 not in W or p2 not in W:
+        return "Please select an option"
+    if p2 in W[p1]:
+        return "Player 1 Won!"
+    if p1 in W[p2]:
+        return "Player 2 Won!"
 
-    Args:
-        taille (_type_): _description_
-        poids (_type_): _description_
-
-    Returns:
-        _type_: _description_
-    """return res
+    return "Draw!"
+print(rpsls("SCISSORS",'PAPER'))
