9810e4b500bdb35aa3325d77eaf178ab7938291c
MagomedORLEANS <magomed.arsamerzoev@etu.univ-orleans.fr>
2024-09-25 23:17:19 +0200

    caca

--- INNIT_PROJ/TP/TP4/tp4_sources.py
+++ INNIT_PROJ/TP/TP4/tp4_sources.py
@@ -1,4 +1,3 @@
-
 """ 
 #exo 2
 def plus_long_plateau(liste_villes, population):
@@ -13,17 +12,26 @@ def plus_long_plateau(liste_villes, population):
 print(plus_long_plateau(["Blois", "Bourges", "Chartres", "Châteauroux", "Dreux","Joué-lès-Tours", "Olivet", "Orléans", "Tours", "Vierzon"], [45871, 64668,  38426, 43442, 30664, 38250, 22168, 116238, 136463,25725]))
 """
 #exo 3
+"""""
 def intwannbe(nombre):
     nb = 0
-    chiffres="0123456789"
-    for i in range(len(nombre)) :
-        for carac in nombre :
-            if carac in nombre and carac in chiffres  :
-                nb = nb *10 + i
+    chiffres = "0123456789"
 
-    return nb
-    
+    for carac in nombre:
+        for i in range(len(chiffres)):
+            if carac == chiffres[i]:
+                nb = nb * 10 + i
+                break #J'ai trouver cette fonction sur internet et elle permet de sauver un peu de temps de calcul, par exemple ici tout les chiffres sont unique donc pas besoin de faire tourner la boucle à chaque fois si cette dernière à déjà été trouver une fois
+
+    returnzz nb
+
 print(intwannbe("2021"))
-            
-        
-        
+"""
+#exo4
+def trv_mot(liste, lettre):
+    listset = []
+    for i in range(len(liste)):
+        for carac in liste[i] :
+            if lettre  == carac[0] :
+                listset.append(liste[i])
+    return listset 
