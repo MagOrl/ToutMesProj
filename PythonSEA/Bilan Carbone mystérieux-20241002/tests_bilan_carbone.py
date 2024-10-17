a1aaf61316417757fead7b80aaabbe4ae58fa029
MagomedORLEANS <magomed.arsamerzoev@etu.univ-orleans.fr>
2024-10-17 22:04:20 +0200

    caca

--- PythonSEA/Bilan Carbone mystérieux-20241002/tests_bilan_carbone.py
+++ PythonSEA/Bilan Carbone mystérieux-20241002/tests_bilan_carbone.py
@@ -80,9 +80,6 @@ def test_recherche_activite_dichotomique():
     assert bc.recherche_activite_dichotomique('Lucas', '2024-09-01', 'type3', []) == None
     assert bc.recherche_activite_dichotomique('Lucas', '2024-09-01', 'type3', [('Lucas', '2024-09-01', 67.2, 'type3'), ('Lucas', '2024-09-02', 70.08, 'type3')]) == ('Lucas', '2024-09-01', 67.2, 'type3')
 
-#def test_charger_sauver():
- #   assert bc.charger_sauver()
-
 def test_temps_activite():
     assert bc.temps_activite(('Lucas', '2024-09-01', 67.2, 'type3'), bc.co2_minute) == 67.2/0.96
     assert bc.temps_activite(('Lucas', '2024-09-02', 70.08, 'type5'), bc.co2_minute) is None
