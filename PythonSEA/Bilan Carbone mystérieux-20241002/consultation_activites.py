169e1a936de0f70f297aa3c1a464fa8939dd8314
MagomedORLEANS <magomed.arsamerzoev@etu.univ-orleans.fr>
2024-10-26 19:29:00 +0200

    ceci

--- PythonSEA/Bilan Carbone mystérieux-20241002/consultation_activites.py
+++ PythonSEA/Bilan Carbone mystérieux-20241002/consultation_activites.py
@@ -2,7 +2,7 @@ import bilan_carbone as bc
 # Ici vos fonctions dédiées aux interactions
 def affichage_menu(liste_options):
     print("+--------------------------------------+")
-    print(" Choisi les informations que tu désire |")
+    print(" Choisi les options que tu désire      |")
     print("+--------------------------------------+")
     for i in range(len(liste_options)):
         print(i + 1, "-->", liste_options[i])
@@ -18,34 +18,147 @@ def nombreentrer(message, nombremax):
                 print(f"Veuillez entrer un nombre entre 1 et {nombremax}.")
         except ValueError:
             print("Veuillez entrer un nombre valide.")
-
 def menu(liste_options):
     affichage_menu(liste_options)
     return nombreentrer("Entrez votre choix ", len(liste_options))
-  
-                    
-                    #########################################################################################################""
+def indepth(liste_info):
+    print("+--------------------------------------+")
+    print(" Choisi les infos que tu désire        |")
+    print("+--------------------------------------+")
+    print("les chiffres dans les paranthèse signifie le nombre de liste que vous avez besoin")
+    for i in range(len(liste_info)):
+        print(i + 1, "-->", liste_info[i])
+def nombreindepth(message, nombremax):
+    messagestr = message + "[1-" + str(nombremax) + "]" + "\n"
+    while True:
+        try:
+            repinfo = int(input(messagestr))
+            if 1 <= repinfo <= nombremax:
+                return repinfo
+            else:
+                print(f"Veuillez entrer un nombre entre 1 et {nombremax}.")
+        except ValueError:
+            print("Veuillez entrer un nombre valide.")
+def menuindepth(liste_info):
+    affichage_menu(liste_info)
+    return nombreentrer("Entrez votre choix ", len(liste_info))
 # Ici votre programme principal
 def programme_principal():
-    liste_options = ["Recherche d'une personne",
-                     "charger un fichier",
-                     "fusionner 2 liste ", 
+    liste_options = ["Recherche d'info",
+                     "Sauvgarder les info dans un fichiers CSV",
+                     "Voir vos listes sauvgarder", 
                      "Quitter"]
+    liste_info =["Recherche par nom(1)",
+                 "Teste si 2 liste est bien avant l'autre(2)","Donne le maximum d'une emission(1)",
+                 "Fusion de listes(2)","Recherche dichitomique(1)"]
+    saved = {}
     quitter = False 
     while not quitter:
         rep = menu(liste_options)
         print("Vous avez choisi : [", liste_options[rep - 1],"]")
         if rep == 1:
-                para = input("Mettre un prenom de la liste" + "\n")
-                print(f"Voici toutes les activité liée à {para}" +"\n")
-                print(bc.filtre_par_prenom(bc.charger_activites(('./PythonSEA/Bilan Carbone mystérieux-20241002/emission.csv')),para))
-                if bc.filtre_par_prenom(bc.charger_activites(('./PythonSEA/Bilan Carbone mystérieux-20241002/emission.csv')),para) == []:
-                    print("la recherche n'a pas abouti"+"\n"+ "Le prénom n'est pas dans la liste "+ "\n"+"Ou vous avez surement mal tapper le prénom, un prénom commence par une majuscule et ne contient aucun espace")
-                
+            repinfo =  menuindepth(liste_info)
+            print("Vous avez choisi : [", liste_options[repinfo - 1],"]")
+            if repinfo == 1:
+                if len(saved) != 0:
+                    question = input("voulez vous utiliser l'une de vos liste[Y/N]"+"\n" "si vous repondez [N]on nous allons utiliser la grande liste"+"\n")
+                    if question == "Y":
+                        clef = input("Quelle liste voulez vous mettre ?"+"\n")
+                        para = input("Mettre un prenom de la liste" + "\n")
+                        for keys,valeur in saved.items():
+                            if clef == keys:
+                                valoc = valeur
+                            if bc.filtre_par_prenom(valoc, para) == []:
+                                print("> la recherche n'a pas abouti:"+"\n"+ "  indice : Le prénom n'est pas dans la liste "+ "\n"+"  indice : Vous avez surement mal tapper le prénom, un prénom commence par une majuscule et ne contient aucun espace")
+                            else:
+                                print(f"Voici toutes les activité liée à {para}" +"\n")
+                                varloc = bc.filtre_par_prenom(valoc,para)
+                                print(varloc)
+                                listYN = input("Voulez vous sauvgarder les information ? [Y/N]"+"\n")
+                                if listYN == "N":
+                                    pass 
+                                elif listYN == "Y":
+                                    nomlist = input("donner un nom a donner à la variable"+"\n")
+                                    saved[nomlist] = varloc
+                                    print("Jusqu'à la fin de votre session, le nom de la liste se nomera " + nomlist)
+                                else:
+                                    print("réponse par Y pour Oui et N pour Non")
+                else :
+                    para = input("Mettre un prenom de la liste" + "\n")
+                    if bc.filtre_par_prenom(bc.charger_activites(('./PythonSEA/Bilan Carbone mystérieux-20241002/emission.csv')),para) == []:
+                        print("> la recherche n'a pas abouti:"+"\n"+ "  indice : Le prénom n'est pas dans la liste "+ "\n"+"  indice : Vous avez surement mal tapper le prénom, un prénom commence par une majuscule et ne contient aucun espace")
+                    else :
+                        print(f"Voici toutes les activité liée à {para}" +"\n")
+                        varloc = bc.filtre_par_prenom(bc.charger_activites(('./PythonSEA/Bilan Carbone mystérieux-20241002/emission.csv')),para)
+                        print(varloc)
+                        listYN = input("Voulez vous sauvgarder les information ? [Y/N]"+"\n")
+                        if listYN == "N":
+                            pass 
+                        elif listYN == "Y":
+                            nomlist = input("donner un nom a donner à la variable"+"\n")
+                            saved[nomlist] = varloc
+                            print("Jusqu'à la fin de votre session, le nom de la liste se nomera " + nomlist)
+                        else:
+                            print("réponse par Y pour Oui et N pour Non")
+            if repinfo == 2:
+                if len(saved) >= 2:
+                    clay1 = input("Quelle est la premiere liste que voulez vous mettre ?"+"\n")
+                    clay2 = input("Quelle est la deuxième liste que voulez vous mettre ?"+"\n")
+                    for keys,valeur in saved.items():
+                        if clay1 == keys:
+                            varloc1 = valeur
+                        else:
+                            print("aucune liste de ce nom")
+                    for key, valor in saved.items():
+                        if clay2 == key:
+                            varloc2 = valor
+                        else:
+                            ("aucune liste de ce nom")
+                    if varloc1 and varloc2:
+                        tar = bc.est_avant(varloc1,varloc2)
+                        if tar == False: 
+                            print(f"Votre liste {clay1} n'est pas avant votre liste {clay2}")
+                        else:
+                            print(f"Votre liste {clay1} est avant votre liste {clay2}")
+                else:
+                    print("il faut sauvgarder plus de 2 listes")
+            if repinfo == 3 :
+                if len(saved) != 0:
+                    question = input("voulez vous utiliser l'une de vos liste[Y/N]"+"\n" "si vous repondez [N]on nous allons utiliser la grande liste"+"\n")
+                    if question == "Y":
+                        clef = input("Quelle liste voulez vous mettre ?")
+                        for keys,valeur in saved.items():
+                            if clef == keys:
+                                valoc = valeur
+                        print(f"la consomation max est {bc.max_emmission(valoc)}")
+                    if question == "N":
+                        print(f"la consomation max est{bc.max_emmission(bc.charger_activites(('./PythonSEA/Bilan Carbone mystérieux-20241002/emission.csv')))}")
+                else:
+                    print(f"la consomation max est{bc.max_emmission(bc.charger_activites(('./PythonSEA/Bilan Carbone mystérieux-20241002/emission.csv')))}")
+
+                                    
+
+
+        if rep == 2:
+            if len(saved) == 0:
+                print("enregistrer des listes d'abord")
+            else:
+                nomfic = input("Comment voulez vous appeler le fichier?"+"\n")
+                kle = input("Quelle liste voulez vous mettre ?"+"\n")
+                for keys,valeur in saved.items():
+                    if kle == keys:
+                        bc.sauver_activites(f'./PythonSEA/Bilan Carbone mystérieux-20241002/{nomfic}.csv', valeur)
+                    else: 
+                        print("Veuillez mettre un bon nom de clef")
+                        pass            
+        if rep == 3:
+            print("Voici les listes sauvegardées :")
+            for keys, value in saved.items():
+                print(f"{keys}")
         if rep == len(liste_options):
             quitter = True
-        
         input("Appuyer sur Entrée pour continuer")
     print("Merci au revoir!")
+
 #appel de fonction
 programme_principal() 
--- PythonSEA/Bilan Carbone mystérieux-20241002/testisole.py
+++ PythonSEA/Bilan Carbone mystérieux-20241002/testisole.py
@@ -1,46 +1,13 @@
 
-# Ici vos fonctions dédiées aux interactions
-def affichage_menu(liste_options):
-    print("+--------------------------------------+")
-    print(" Choisi les informations que tu désire |")
-    print("+--------------------------------------+")
-    for i in range(len(liste_options)):
-        print(i+1,"-->",liste_options[i])
-def nombreentrer(message,nombremax):
-    messagestr = message + "[1-" +  str(nombremax) + "]" +"\n"
-    while True:
-        rep = int(input(messagestr))
-        if 1<= rep <= nombremax:
-            return rep 
-        else:
-            return None  
-
-def menu(liste_options):
-    affichage_menu(liste_options)
-    return nombreentrer("Entrez votre choix", len(liste_options))
-     
-
-# ici votre programme principal
-def programme_principal():
-    liste_options = ["Charger un fichier",
-                     "Rechercher la population d'une commune",
-                     "Afficher la population d'un département", 
-                     "Quitter"]
-    quitter = False 
-    while not quitter:
-        rep = menu(liste_options)
-        if rep is None:
-            print("Cette fonctions n'existe pas, entrez vos réponses de manière cohérente S.V.P")
-        elif rep == 1:
-            print("Vous avez choisi", liste_options[rep - 1])
-        elif rep == 2:
-            print("Vous avez choisi", liste_options[rep - 1])
-        elif rep == 3:
-            print("Vous avez choisi", liste_options[rep - 1])
-        elif rep == len(liste_options):
-            print("Vous avez choisi", liste_options[rep - 1])
-            quitter = True
-        input("Appuyer sur Entrée pour continuer")
-    print("Merci au revoir!")
-
-programme_principal()
+if len(saved) != 0:
+                    question = input("voulez vous utiliser l'une de vos liste[Y/N]"+"\n" "si vous repondez [N]on nous allons utiliser la grande liste"+"\n")
+                    if question == "Y":
+                        clef = input("Quelle liste voulez vous mettre ?")
+                        for keys,valeur in saved.items():
+                            if clef == keys:
+                                valoc = valeur
+                        print(f"la consomation max est {bc.max_emmission(valoc)}")
+                    if question == "N":
+                        print(f"la consomation max est{bc.max_emmission(bc.charger_activites(('./PythonSEA/Bilan Carbone mystérieux-20241002/emission.csv')))}")
+                else:
+                    print(f"la consomation max est{bc.max_emmission(bc.charger_activites(('./PythonSEA/Bilan Carbone mystérieux-20241002/emission.csv')))}")
