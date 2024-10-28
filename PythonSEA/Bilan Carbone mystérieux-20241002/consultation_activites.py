ef2d94a1280380abeeadc5fc91b63147c2876c03
MagomedORLEANS <magomed.arsamerzoev@etu.univ-orleans.fr>
2024-10-28 13:01:46 +0100

    caca

--- PythonSEA/Bilan Carbone mystérieux-20241002/consultation_activites.py
+++ PythonSEA/Bilan Carbone mystérieux-20241002/consultation_activites.py
@@ -42,14 +42,14 @@ def menuindepth(liste_info):
 def programme_principal():
     liste_options = ["Recherche d'info",
                      "Sauvgarder les info dans un fichiers CSV",
-                     "Voir vos listes sauvgarder", 
+                     "Voir vos listes sauvgarder","Comment utiliser le programme",
                      "Quitter"]
     liste_info =["Recherche par nom(1*)",
                  "Teste si 2 liste est croissant(2)","Donne le maximum d'une emission(1)",
                  "Fusion de listes(2*)","Recherche(N/A)","Liste de personne uniquement(1)","Cumul du temps d'activité d'une liste(1)","date de première apparition d'un type(1)",
                  "filtres selon plusieures critères(1*)","Plus longue suite d'émission décroissante(1)","Verifie si liste est trier(1)","Liste de types uniquement(1)","Temps d'activité pour 1 seule profile(N/A)",
                  "Savoir l'année d'un profile(N/A)","Savoir l'année et le mois d'un profile(N/A)","Emission totale d'une liste(1)",
-                 "Quitter \n le chiffre entre paranthèse signifie le nombre de liste qu'il faut avoir sauvgarder au préalable \n pour les options ou on à besoin de qu'une seule liste il est aussi possible de le faire avec la grande liste(c'est comme ca qu'on enregistres des listes) \n les '*' signifie que l'option propose d'enregistrer la liste obtenu pour la manipuler par la suite"]
+                 "Retour au menu\n le chiffre entre paranthèse signifie le nombre de liste qu'il faut avoir sauvgarder au préalable \n pour les options ou on à besoin de qu'une seule liste il est aussi possible de le faire avec la grande liste(c'est comme ca qu'on enregistres des listes) \n les '*' signifie que l'option propose d'enregistrer la liste obtenu pour la manipuler par la suite"]
     saved = {}
     quitter = False 
     while not quitter:
@@ -117,43 +117,48 @@ def programme_principal():
                                 saved[nomlist] = varloc
                                 print("Jusqu'à la fin de votre session, le nom de la liste se nomera " + nomlist)
                             else:
-                                print("réponse par Y pour Oui et N pour Non")
-                            
+                                print("réponse par Y pour Oui et N pour Non")                     
                 if repinfo == 2:
                     if len(saved) >= 2:
-                        clay1 = input("Quelle est la premiere liste que voulez vous mettre ?"+"\n")
-                        clay2 = input("Quelle est la deuxième liste que voulez vous mettre ?"+"\n")
-                        for keys,valeur in saved.items():
-                            if clay1 == keys:
-                                varloc1 = valeur
-                            else:
-                                print("aucune liste de ce nom")
-                        for key, valor in saved.items():
-                            if clay2 == key:
-                                varloc2 = valor
-                            else:
-                                ("aucune liste de ce nom")
-                        if varloc1 and varloc2:
-                            tar = bc.est_avant(varloc1,varloc2)
-                            if tar == False: 
-                                print(f"Votre liste {clay1} n'est pas avant votre liste {clay2}")
-                            else:
-                                print(f"Votre liste {clay1} est avant votre liste {clay2}")
+                        try :
+                            clay1 = input("Quelle est la premiere liste que voulez vous mettre ?"+"\n")
+                            clay2 = input("Quelle est la deuxième liste que voulez vous mettre ?"+"\n")
+                            for keys,valeur in saved.items():
+                                if clay1 == keys:
+                                    varloc1 = valeur
+                                else:
+                                    print("aucune liste de ce nom")
+                            for key, valor in saved.items():
+                                if clay2 == key:
+                                    varloc2 = valor
+                                else:
+                                    ("aucune liste de ce nom")
+                            if varloc1 and varloc2:
+                                tar = bc.est_avant(varloc1,varloc2)
+                                if tar == False: 
+                                    print(f"Votre liste {clay1} n'est pas avant votre liste {clay2}")
+                                else:
+                                    print(f"Votre liste {clay1} est avant votre liste {clay2}")
+                        except:
+                            print("ERREUR : Vous avez surement mal tapper l'une de vos 2 listes ou les 2, retourner sur le menu pour voir le nom de vos listes ")
                     else:
                         print("il faut sauvgarder plus de 2 listes")
                 if repinfo == 3 :
                     if len(saved) != 0:
                         question = input("voulez vous utiliser l'une de vos liste[Y/N]"+"\n" "si vous repondez [N]on nous allons utiliser la grande liste"+"\n")
                         if question == "Y":
-                            clef = input("Quelle liste voulez vous mettre ?")
-                            for keys,valeur in saved.items():
-                                if clef == keys:
-                                    valoc = valeur
-                            print(f"la consomation max est {bc.max_emmission(valoc)}")
+                            try :
+                                clef = input("Quelle liste voulez vous mettre ?\n")
+                                for keys,valeur in saved.items():
+                                    if clef == keys:
+                                        valoc = valeur
+                                print(f"la consomation maximal de votre liste {clef} est {bc.max_emmission(valoc)}")
+                            except:
+                                print("Recherche n'a pas abouti vous avez surement mal tapper le nom de votre liste, retourner dans le menu principal et choissisez la 3ème option pour avoir le nom de vos listes ")
                         if question == "N":
-                            print(f"la consomation max est{bc.max_emmission(bc.charger_activites(('./PythonSEA/Bilan Carbone mystérieux-20241002/emission.csv')))}")
+                            print(f"la consomation maximal est{bc.max_emmission(bc.charger_activites(('./PythonSEA/Bilan Carbone mystérieux-20241002/emission.csv')))}")
                     else:
-                        print(f"la consomation max est{bc.max_emmission(bc.charger_activites(('./PythonSEA/Bilan Carbone mystérieux-20241002/emission.csv')))}")
+                        print(f"la consomation maximale est{bc.max_emmission(bc.charger_activites(('./PythonSEA/Bilan Carbone mystérieux-20241002/emission.csv')))}")
                 if repinfo == 4:
                     if len(saved) >= 2:
                         clay1 = input("Quelle est la premiere liste que voulez vous mettre ?"+"\n")
@@ -175,7 +180,7 @@ def programme_principal():
                             pass 
                         elif listYN == "Y":
                             nomlist = input("donner un nom a donner à la variable"+"\n")
-                            saved[nomlist] = varloc
+                            saved[nomlist] = bc.fusionner_activites(varlocal1,varlocal2)
                             print("Jusqu'à la fin de votre session, le nom de la liste se nomera " + nomlist)
                         else:
                             print("réponse par Y pour Oui et N pour Non")
@@ -187,28 +192,26 @@ def programme_principal():
                         if question == "Y":
                             cle = input("quelle liste voulez vous mettre \n")
                             paranom =input("Quelle nom cherchez vous ?\n")
-                            parajour = input("Quelle jour cherchez vous ?"+"\n"+'format: "anne-mm-jj"\n') 
+                            parajour = input("Quelle date cherchez vous ?"+"\n"+'format: "anne-mm-jj"\n') 
                             paratype = input("Quelle type cherchez vous ? \n")
-                            for keys, valeur in saved.items():
-                                if cle == keys:
-                                    valoc = valeur
                             print(f"Voici la trouvaille : {bc.recherche_activite_dichotomique(paranom,parajour,paratype,valoc)}\n")
                         if question == "N":
-                            paranom =input("Quelle nom cherchez vous ?\n")
-                            parajour = input("Quelle jour cherchez vous ?"+"\n"+'format: "anne-mm-jj"\n') 
-                            paratype = input("Quelle type cherchez vous ? \n")
-                            for keys, valeur in saved.items():
-                                if clef == keys:
-                                    valoc = valeur
-                            print(f"Voici la trouvaille : {bc.recherche_activite_dichotomique(paranom, parajour ,paratype ,bc.charger_activites(('./PythonSEA/Bilan Carbone mystérieux-20241002/emission.csv')))}\n")
+                            try :
+                                paranom =input("Quelle nom cherchez vous ?\n")
+                                parajour = input("Quelle date cherchez vous ?"+"\n"+'format: "anne-mm-jj"\n') 
+                                paratype = input("Quelle type cherchez vous ? \n")
+                                print(f"Voici la trouvaille : {bc.recherche_activite_dichotomique(paranom, parajour ,paratype ,bc.charger_activites(('./PythonSEA/Bilan Carbone mystérieux-20241002/emission.csv')))}\n")
+                            except:
+                                print("L'une des informations que vous avez rentrer est fallacieuse ")
                     else:
-                        paranom = input("Quelle nom cherchez vous ? \n")
-                        parajour = input("Quelle jour cherchez vous ?"+"\n"+'format: "anne-mm-jj"\n') 
-                        paratype = input("Quelle type cherchez vous ? \n")
-                        for keys, valeur in saved.items():
-                            if clef == keys:
-                                valoc = valeur                    
-                        print(f"Voici la trouvaille : {bc.recherche_activite_dichotomique(paranom, parajour ,paratype ,bc.charger_activites(('./PythonSEA/Bilan Carbone mystérieux-20241002/emission.csv')))}\n")
+                        try:
+                            paranom = input("Quelle nom cherchez vous ? \n")
+                            parajour = input("Quelle date cherchez vous ?"+"\n"+'format: "anne-mm-jj"\n') 
+                            paratype = input("Quelle type cherchez vous ? \n")                
+                            print(f"Voici la trouvaille : {bc.recherche_activite_dichotomique(paranom, parajour ,paratype ,bc.charger_activites(('./PythonSEA/Bilan Carbone mystérieux-20241002/emission.csv')))}\n")
+                        except:
+                            print("L'une des informations que vous avez rentrer est fallacieuse ")
+
                 if repinfo == 6:
                     if len(saved) != 0:
                         question = input("voulez vous utiliser l'une de vos liste[Y/N]"+"\n" "si vous repondez [N]on nous allons utiliser la grande liste"+"\n")
@@ -435,7 +438,7 @@ def programme_principal():
             question = input("Voulez vous le contenu de votre liste ?[Y/N]"+"\n")
             if question == "Y":
                 for key,valeur in saved.items():
-                    question2 = input("quelle liste voulez vous savoir la valeur de ?"+"\n"+"appuyer sur entrer pour passer (pour que le nom de la liste qui vous intéresse coincide avec sont indexe"+ "\n"+ "par exemple si la liste qui vous interesse se trouve a la 3 ème place appuyer sur entrer 3 fois et écrivez sont nom)"+"\n")
+                    question2 = input("quelle liste voulez vous savoir la valeur de ?"+"\n"+"appuyer sur entrer pour passer (pour que le nom de la liste qui vous intéresse coincide avec sont indexe"+ "\n"+ "par exemple si la liste qui vous interesse se trouve a la 3 ème place appuyer sur entrer 2 fois et écrivez sont nom)"+"\n")
                     if question2 == key :
                         print(valeur)
                         print(f"voici le contenu de votre liste {question2}")
@@ -443,7 +446,8 @@ def programme_principal():
                         print("aucune liste de ce nom")
             elif question == "N":
                 pass
-
+        if rep == 4:
+            print("Il y'a plusieurs étapes pour pouvoir plainement utiliser le programme:\n d'abord faite enregistrer des listes grâce au fichier CSV fourni(contenant toutes les informations), les options qui propose de sauvgarder vos listes  sont marquer d'une étoile '*' \n cela va créée une mémoire dynamique qui contient votre liste (grâce au dictionnaire), à noter que vous pouvez aussi nommer les listes comme vous le shouaitez \n ensuite une fois que avez enregistrer des listes vous pourrez les manipuler (il vous sera demander si oui ou non vous voulez utilser l'une de vos listes pour lancer une option, si vous repondez non ça va lancer le programme avec le fichier CSV), \n ensuite quand vous aurez fini, revenez dans le menu principale pour sauvgarder vos listes en fichiers CSV (fichier CSV que vous pouvez nommer à votre guise)")
         if rep == len(liste_options):
             quitter = True
         input("Appuyer sur Entrée pour continuer")
