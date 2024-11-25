fd86738281405fc1acb9f0d22ae28d5954b2b731
MagomedORLEANS <magomed.arsamerzoev@etu.univ-orleans.fr>
2024-11-25 19:40:39 +0100

    caca

---
+++ INNIT_PROJ/CodeWarCodesISolved.py
@@ -0,0 +1,12 @@
+#some codewar code I solved me happy :)
+
+"""Pete likes to bake some cakes. He has some recipes and ingredients.
+ Unfortunately he is not good in maths. Can you help him to find out, how many cakes he could bake considering his recipes?"""
+def cakes(recipe, available):
+    seto = set()
+    for clef, valeur in recipe.items():
+        if clef in available.keys():
+            seto.add(available[clef] // valeur)  
+        else:
+            return 0
+    return min(seto)
--- INNIT_PROJ/TP/sante.py
+++
@@ -1,7 +0,0 @@
-def lemax(x):
-    o = x[0]
-    for i in range(len(x)):
-        if o < x[i]:
-            o = x 
-    return o 
-
