a60be881a9ef1cb4dafe16f25bc4181ec9b734af
MagomedORLEANS <magomed.arsamerzoev@etu.univ-orleans.fr>
2024-12-10 10:36:00 +0100

    caca

--- INNIT_PROJ/TP/TP10/ecosysteme/ecosysteme.py
+++ INNIT_PROJ/TP/TP10/ecosysteme/ecosysteme.py
@@ -2,25 +2,35 @@
 Init Dev : TP10
 Exercice 2 : Ecosystème
 """
+ecosysteme_1 = { 'Loup': 'Mouton', 'Mouton':'Herbe', 'Dragon':'Lion', 'Lion':'Lapin', 'Herbe':None, 'Lapin':'Carotte', 'Requin':'Surfer'}
 
 def extinction_immediate(ecosysteme, animal):
     """
     renvoie True si animal s'éteint immédiatement dans l'écosystème faute
     de nourriture
     """
-    ...
+    if ecosysteme[animal] in ecosysteme:
+        return False
+    return True 
 
 
 def en_voie_disparition(ecosysteme, animal):
     """
     renvoie True si animal s'éteint est voué à disparaitre à long terme
     """
-    ...
+    var = animal 
+    while ecosysteme[var] is not None:
+        if var in ecosysteme:
+            var = ecosysteme[var]
+        else:
+            return False
+    return True
+print(en_voie_disparition(ecosysteme_1,'Lapin'))
 
 
 def animaux_en_danger(ecosysteme):
     """ renvoie l'ensemble des animaux qui sont en danger d'extinction immédiate"""
-    ...
+    
 
 
 def especes_en_voie_disparition(ecosysteme):
