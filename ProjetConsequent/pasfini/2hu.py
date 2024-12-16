ea39c911bac9ba525c61dc10cae122c80659e948
MagOrl <magomed.arsamerzoev@etu-orleans.fr>
2024-12-16 04:48:40 +0100

    caca

--- ProjetConsequent/pasfini/2hu.py
+++ ProjetConsequent/pasfini/2hu.py
@@ -1,13 +1,18 @@
 import pygame
 from sys import exit 
+from pygame.locals import *
+
+
 pygame.init()
-screen = pygame.display.set_mode(((800,400)))
-pygame.display.set_caption('Touhou : the Embodiment of the Python Devils')
+screen = pygame.display.set_mode(((1020,720)))
+imgtest = pygame.image.load('ProjetConsequent/pasfini/flandre.png')
+background = 'dark gray'
 clock = pygame.time.Clock()
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+    screen.fill(background)
     pygame.display.update()
-    clock.tick(60)      
+    clock.tick(60)
