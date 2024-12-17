e377ea77292ba0148ea4609ad6a3253d82a7dbfe
MagomedORLEANS <magomed.arsamerzoev@etu.univ-orleans.fr>
2024-12-17 10:58:44 +0100

    caaca

--- INNIT_PROJ/TP/TP11/TP11.py
+++ INNIT_PROJ/TP/TP11/TP11.py
@@ -1,3 +1,4 @@
+import api_matrice as mat
 ATM={'Armand':'Beatrice','Beatrice':'Cesar','Cesar':'Dalida','Dalida':'Cesar','Etienne':'Beatrice','Firmin':'Henri', 'Gaston':'Beatrice','Henri':'Firmin'}
 
 def amourreciprok(dico):
@@ -17,3 +18,25 @@ def soupirant(personne, dico):
             personneamoureu.append(elem)
     return personneamoureu
 
+def sous_matrice(matrice, nb_lignes, nb_colonnes, position_haut, position_gauche):
+    
+    mato = mat.creer_matrice(nb_lignes,nb_colonnes)
+    
+    for i in range (nb_lignes):
+        for j in range(nb_colonnes):
+            mat.set_valeur(mato,i,j,matrice[position_haut + i ][position_gauche + j])
+    return mato 
+  
+
+mati = [[1,2,3],[4,5,6],[7,8,9]]
+print(sous_matrice(mati,2,2,0,0))
+
+def colle_sous_matrice(matrice,sous_matrice,ligne_haut,colonne_gauche):
+    for i in range(len(sous_matrice)):
+        for j in range(len(sous_matrice[1])):
+            matrice[ligne_haut + i][colonne_gauche +j] = sous_matrice[i][j]
+    return matrice 
+
+sousmat = [[67, 42], [43, 17]]
+
+print(colle_sous_matrice(mati,sousmat,0,0))
