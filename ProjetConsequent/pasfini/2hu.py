d6241a18411e97145fe93974ee8b6a75125606c0
MagOrl <magomed.arsamerzoev@etu-orleans.fr>
2024-12-18 20:18:10 +0100

    caca

--- ProjetConsequent/pasfini/2hu.py
+++ ProjetConsequent/pasfini/2hu.py
@@ -7,33 +7,29 @@ screen = pygame.display.set_mode(((1020,720)))
 background = 'gray'
 clock = pygame.time.Clock()
 
-class girl:
-    def __init__(info,x,y,vel,nb_life,nb_bomb,xp,lvl):
+class girl(object):
+    def __init__(info,x,y,vel,nb_life,nb_bomb,xp,lvl,hitbox):
          info.nb_life = nb_life 
          info.nb_bomb = nb_bomb
          info.xp = xp 
          info.lvl = lvl
          info.x = x 
          info.y = y 
+         info.hitbox = hitbox
+         info.vel = vel
     def __str__(info):
         return f"Life : {info.nb_life}\n Bomb : {nb_bomb} \n Power :{info.xp}"
-class projectile: 
+class projectile(object): 
     def __init__(self,x,y,width,height,vel):
         self.x = x 
         self.y = y 
         self.width = width
         self.height = height
         self.vel = vel 
-    def draw(self,screen):
-        pygame.draw.rect(screen,'blue',(x,self.vel,5,5))
+    
 
-x = 400
-y= 200
-rad = 5
-vel = 7
 
-projvel = 8
-projtire = y 
+carac = girl(400,200,7,3,3,0,0,4)
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -42,31 +38,29 @@ while True:
     clef = pygame.key.get_pressed()
      
         
-    if clef[pygame.K_LEFT] and x > rad:
+    if clef[pygame.K_LEFT] and carac.x > carac.hitbox:
         if clef[pygame.K_LSHIFT]:
-            x-= shiftvel//2
+            carac.x-= carac.vel // 2
         else:
-            x -= vel 
-    if clef[pygame.K_RIGHT] and x<1020-rad: 
+            carac.x -= carac.vel 
+    if clef[pygame.K_RIGHT] and carac.x < 1020 - carac.hitbox: 
         if clef[pygame.K_LSHIFT]:
-            x += shiftvel//2
+            carac.x += carac.vel // 2
         else:
-            x += vel 
-    if clef[pygame.K_UP] and y>rad:           
+            carac.x += carac.vel 
+    if clef[pygame.K_UP] and carac.y > carac.hitbox:           
         if clef[pygame.K_LSHIFT]:
-            y -= shiftvel//2
+            carac.y -= carac.vel // 2
         else:
-            y -= vel 
-    if clef[pygame.K_DOWN] and y<720-rad: 
+            carac.y -= carac.vel 
+    if clef[pygame.K_DOWN] and carac.y < 720 - carac.hitbox: 
         if clef[pygame.K_LSHIFT]:
-            y += shiftvel//2
+            carac.y += carac.vel // 2
         else:
-            y += vel 
-    
+            carac.y += carac.vel 
     screen.fill(background)
-    pygame.draw.circle(screen, 'red', [x,y], rad) 
-    if clef[pygame.K_w]:
-        
-
+    pygame.draw.circle(screen, 'red', [carac.x,carac.y], carac.hitbox) 
+    pygame.draw.rect(screen,'blue',(carac.x,projectile()))
+    #pygame.draw.rect(win, (255, 0, 0), (x, y, width, height))
     pygame.display.update()
     clock.tick(60)
